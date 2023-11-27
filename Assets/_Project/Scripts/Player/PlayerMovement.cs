f02c58825a0b198fa3b97e083a51a14c19b1df97
Alyson Morais <alysonmorais35@hotmail.com>
2023-11-27 20:56:17 -0300

    Implement player movement

---
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Alterables")]
+    [SerializeField] private float _movementSpeed = 10;
+    [SerializeField] private float _rotationSpeed = 50;
+
+    private Vector3 _movement;
+    private Vector3 _rotation;
+    
+    private void FixedUpdate()
+    {
+        Rotation();
+        Movement();
+    }
+
+    private void Rotation()
+    {
+        Quaternion rotation = transform.rotation;
+        float anglesZ = rotation.eulerAngles.z;
+        anglesZ -= _rotation.x * _rotationSpeed * Time.deltaTime;
+        rotation = Quaternion.Euler(0, 0, anglesZ);
+        transform.rotation = rotation;
+    }
+
+    private void Movement()
+    {
+        Vector3 position = transform.position;
+        Vector3 velocity = new Vector3(0, _movement.y * _movementSpeed * Time.deltaTime, 0);
+        position += transform.rotation * velocity;
+        transform.position = position;
+    }
+    
+    public void SetMovement(InputAction.CallbackContext value)
+    {
+        _movement = value.ReadValue<Vector2>();
+    }
+    
+    public void SetRotation(InputAction.CallbackContext value)
+    {
+        _rotation = value.ReadValue<Vector2>();
+    }
+}
